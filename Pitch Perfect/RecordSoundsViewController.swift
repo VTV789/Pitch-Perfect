491c73be3758b5747938419756d18b89c0d0f842
Vinh Thanh Vu <vtv@vinh.com>
2015-11-19 23:45:27 -0500

    Code for efficient storage

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -41,10 +41,8 @@ class RecordSoundsViewController: UIViewController {
         // TODO: Record the user's voice
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
         
-        let currentDateTime = NSDate()
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = "ddMMyyyy-HHmmss"
-        let recordingName = formatter.stringFromDate(currentDateTime)+".wav"
+
+        let recordingName = "my_audio.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = NSURL.fileURLWithPathComponents(pathArray)
         print(filePath)
@@ -60,6 +58,9 @@ class RecordSoundsViewController: UIViewController {
     
     @IBAction func stopAudio(sender: UIButton) {
         recordingInProgress.hidden = true
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
     }
     
 }
